e87263f39442c8be94f801811e52034469d39df7
akshat <akshat.tandon@tum.de>
2024-12-01 12:55:21 +0100

    Day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,41 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val listLeft = mutableListOf<Int>()
+        val listRight = mutableListOf<Int>()
+
+        for (line in input) {
+            val pair = line.split(Regex("\\s+"))
+            listLeft.add(pair[0].toInt())
+            listRight.add(pair[1].toInt())
+        }
+
+        listLeft.sort()
+        listRight.sort()
+        var sumLists = 0
+        for (i in input.indices) {
+            sumLists += abs(listLeft[i] - listRight[i])
+        }
+
+        return sumLists
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listLeft = mutableListOf<Int>()
+        val hashMapRight = HashMap<Int, Int>()
+
+        for (line in input) {
+            val pair = line.split(Regex("\\s+"))
+            listLeft.add(pair[0].toInt())
+            hashMapRight[pair[1].toInt()] = hashMapRight.getOrPut(pair[1].toInt()) { 0 } + 1
+        }
+
+        val similarityScore = listLeft.fold(0) { sum, element -> sum + element * hashMapRight.getOrElse(element) { 0 } }
+
+        return similarityScore
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
